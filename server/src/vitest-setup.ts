f80a724862e86715c8967e630d193c2c39b21274
Zion Emond <zionem7@gmail.com>
2026-05-11 17:00:25 -0400

    update dev port numbers

--- server/src/vitest-setup.ts
+++ server/src/vitest-setup.ts
@@ -5,7 +5,7 @@
 process.env.NODE_ENV = "test";
 if (!process.env.DATABASE_URL) {
   process.env.DATABASE_URL =
-    "postgresql://postgres:postgres@127.0.0.1:5432/byc_camp_manager";
+    "postgresql://postgres:postgres@127.0.0.1:15432/byc_camp_manager";
 }
 if (!process.env.JWT_SECRET || process.env.JWT_SECRET.length < 32) {
   process.env.JWT_SECRET = "0123456789abcdef0123456789abcdef0123456789";
