0d5db9458dc66361c9f562fa8fbfbba147d06ce8
Zion Emond <zionem7@gmail.com>
2026-05-28 10:05:58 -0400

    make sure to exclude the correct directories from build

--- infra/cdk/bin/app.ts
+++ infra/cdk/bin/app.ts
@@ -7,7 +7,7 @@ const app = new cdk.App();
 const account =
   app.node.tryGetContext("account") ?? process.env.CDK_DEFAULT_ACCOUNT ?? process.env.AWS_ACCOUNT_ID;
 const region =
-  app.node.tryGetContext("region") ?? process.env.CDK_DEFAULT_REGION ?? process.env.AWS_REGION ?? "us-east-1";
+  app.node.tryGetContext("region") ?? process.env.CDK_DEFAULT_REGION ?? process.env.AWS_REGION ?? "us-east-2";
 
 new BycCampDevStack(app, "BycCampDevStack", {
   env: account && region ? { account: String(account), region: String(region) } : undefined,
